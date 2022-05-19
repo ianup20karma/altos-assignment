a69570d44f982e11646b2aa9eaa7b70b0d48ee51
Anup Vishwakarma <ianupvishwakarma@gmail.com>
2022-05-19 14:07:19 +0530

    updated html

--- src/app/notification-list/notification-list.component.ts
+++ src/app/notification-list/notification-list.component.ts
@@ -9,19 +9,23 @@ import Notifications from '../../assets/notifications.json';
 export class NotificationListComponent implements OnInit {
   notifications = Notifications;
   toastCount = 0;
+  toastReadValue: {read: boolean}[] = [];
 
   ngOnInit(): void {
     this.toastCount = this.notifications.length;
+    for (let index = 0; index < this.notifications.length; index++) {
+      this.toastReadValue.push({read: false});
+    }
   }
 
   toastAction(event: any) {
     const collection = document.getElementsByClassName('titleToast' + event.id)[0];
     const index = this.notifications.indexOf(event.toast);
     if (index !== -1) {
-      if (event.action == 'check' || event.action == 'file' && !this.notifications[index].read) {
+      if ((event.action == 'check' || event.action == 'file') && !this.toastReadValue[index].read) {
         collection.setAttribute('style', 'font-weight: 400;');
         this.toastCount -= 1;
-        this.notifications[index].read = true;
+        this.toastReadValue[index].read = true;
       } else if (event.action == 'delete') {
         this.notifications.splice(index, 1);
       }
